2c2471babf63d0f223eccba6e07024505dff1e5c
lozanonicolas <lozanonicolas.c@gmail.com>
2026-08-10 01:19:59 -0300

    feat: add SheetPilot sidebar

--- Code.js
+++ Code.js
@@ -1,3 +1,14 @@
-function myFunction() {
-  
+function onOpen() {
+  SpreadsheetApp.getUi()
+    .createMenu('SheetPilot')
+    .addItem('Open SheetPilot', 'showSidebar')
+    .addToUi();
+}
+
+function showSidebar() {
+  const html = HtmlService
+    .createHtmlOutputFromFile('sidebar')
+    .setTitle('SheetPilot');
+
+  SpreadsheetApp.getUi().showSidebar(html);
 }
